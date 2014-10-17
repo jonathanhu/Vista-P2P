70803c03176e659bc0f37eebc0f286e36123abb1
Carlos Torres <carlos.eztorres@gmail.com>
2014-10-17 15:44:08 -0600

    Inicio de Arreglos con respecto a la ducplicidad de los datos

--- XXSV_FA001_00008/xbol/sql/grant_bolinf.sql
+++ XXSV_FA001_00008/xbol/sql/grant_bolinf.sql
@@ -1,7 +1,11 @@
 grant all on XXSV_PO_REPORT_UTL to apps with grant option;
-grant all on XXSV_P2P_V to apps;
-grant select on XXSV_P2P_V to SV_DWUSER;grant all on GL_CODE_COMBINATIONS_KFV to apps with grant option;
-grant all on FA_BOOKS_V to apps with grant option; 
+
+grant select on XXSV_P2P_V to apps;
+
+grant select on XXSV_P2P_V to SV_DWUSER;
+
+grant select on XXSV_P2P_V to SV_MWUSER;
+
 /
 exit
 /
