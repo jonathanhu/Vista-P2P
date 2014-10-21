20aa0dea5a39b70b36d2984cd91ec33db0118d37
jonathanhu <joro37@hotmail.com>
2014-10-21 10:59:26 -0600

    Adicion de grants apps

--- XXSV_FA001_00008/xbol/sql/grant_apps.sql
+++ XXSV_FA001_00008/xbol/sql/grant_apps.sql
@@ -2,6 +2,38 @@
 
 grant select on APPS.FA_BOOKS_V to bolinf with grant option;
 
+ grant select on  apps.po_headers_all to bolinf with grant option;
+ 
+ grant select on  apps.po_lines_all to bolinf with grant option;
+ 
+ grant select on  apps.po_line_types to bolinf with grant option;
+ 
+ grant select on         apps.ap_terms to bolinf with grant option;
+ 
+ grant select on        apps.po_line_locations_all to bolinf with grant option;
+  
+ grant select on         apps.po_distributions_all to bolinf with grant option;
+ 
+ grant select on         apps.gl_code_combinations_kfv to bolinf with grant option;
+ 
+grant select on          apps.mtl_system_items to bolinf with grant option;
+
+grant select on          apps.ap_suppliers to bolinf with grant option;
+
+grant select on          apps.ap_supplier_sites_all  to bolinf with grant option;
+
+grant select on          apps.per_people_f to bolinf with grant option;
+
+grant select on          apps.invfv_inventory_organizations to bolinf with grant option;
+
+ grant select on         apps.ap_invoice_distributions_all to bolinf with grant option;
+ 
+ grant select on         apps.fa_asset_invoices to bolinf with grant option;
+ 
+grant select on          apps.fa_additions to bolinf with grant option;
+
+grant select on          apps.fa_books_v to bolinf with grant option;
+
 grant select on APPS.GL_CODE_COMBINATIONS_KFV to bolinf with grant option;
 
 
